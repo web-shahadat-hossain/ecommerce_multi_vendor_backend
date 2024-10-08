3b370c1f3bb5a9590207f07439932a13813ccea5
Md Shahadat Hossain <mdridoysikder862@gmail.com>
2024-10-08 22:38:55 +0600

    review  model is done

---
+++ src/models/reviewModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: String,
+    vendorReply: {
+      comment: String,
+      createAt: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+reviewSchema.index({ user: 1, product: 1 }, { unique: true });
+export const Review = mongoose.model("Review", reviewSchema);
