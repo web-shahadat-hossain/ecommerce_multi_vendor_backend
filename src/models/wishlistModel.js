adc225761039b5c7729639304562f2d32db8f96f
Md Shahadat Hossain <mdridoysikder862@gmail.com>
2024-10-08 20:26:03 +0600

    wishlist  model is done

---
+++ src/models/wishlistModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const wishlistSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Wishlist = mongoose.Aggregate.model("wishlist", wishlistSchema);
